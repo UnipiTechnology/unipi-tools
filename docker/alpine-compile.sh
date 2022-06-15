56354ff6c1fd040a58bf69b9839cbe782d68b67f
Miroslav Ondra <bokula@faster.cz>
2022-06-15 13:03:58 +0000

    Add modbus container to docker-compose

--- docker/alpine-compile.sh
+++ docker/alpine-compile.sh
@@ -2,6 +2,6 @@
 
 [ -z "$1" ] && { echo "Requires image name as parameter!" >&2; exit 1; }
 
-docker run -it --rm -v `pwd`/src alpine:3.15 sh -c "cd /src; apk add musl-dev libmodbus-dev make gcc; make clean; make unipi_tcp_server"
+docker run -it --rm -v `pwd`/src:/src alpine:3.15 sh -c "cd /src; apk add musl-dev libmodbus-dev make gcc; make clean; make unipi_tcp_server"
 
 docker build . -t "$1"
