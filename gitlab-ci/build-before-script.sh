300efce173331a2455ebc8598329791c2b8c5a3f
Miroslav Ondra <ondra@faster.cz>
2023-08-24 15:05:49 +0200

    Fix installing raspberrypi-kernel-headers

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -8,6 +8,11 @@ apt update && apt install -y  libmodbus-dev libsystemd-dev libi2c-dev
 
 if [ "${DEBIAN_VERSION}" == "stretch" -o "${DEBIAN_VERSION}" == "buster" ]; then
     ln -s debian.pre debian
+    ARCH="$(dpkg-architecture -q DEB_BUILD_ARCH)"
+    if "$ARCH" = "armhf"; then
+        /ci-scripts/fix-product-repository.sh "${DEBIAN_VERSION}" "${PRODUCT}" "${ARCH}"
+        apt update  && apt-get install -y raspberrypi-kernel-headers
+    fi
 else 
     ln -s debian.n debian
 fi
