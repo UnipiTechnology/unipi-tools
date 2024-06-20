8327afa9985b84aed2a09c776063b5f268b7dddc
Miroslav Ondra <ondra@faster.cz>
2024-06-20 10:52:29 +0000

    Add tests, extend README.md

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -2,6 +2,6 @@
 
 #set -o xtrace
 
-apt update && apt install -y  libmodbus-dev libsystemd-dev libi2c-dev libmhash-dev pkg-config autotools-dev autoconf automake systemd
+apt update && apt install -y  libmodbus-dev libsystemd-dev libmhash-dev pkg-config autotools-dev autoconf automake libtool systemd
 
 . /ci-scripts/include.sh
