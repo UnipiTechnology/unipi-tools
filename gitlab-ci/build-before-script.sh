8c253bd445239769b067994c8e223fa01a070053
Miroslav Ondra <ondra@faster.cz>
2025-05-04 03:19:22 +0200

    feat: Prepare building environment for Trixie

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -2,6 +2,6 @@
 
 #set -o xtrace
 
-apt update && apt install -y  libmodbus-dev libsystemd-dev libmhash-dev pkg-config autotools-dev autoconf automake libtool systemd
+apt update && apt install -y  libmodbus-dev libsystemd-dev libmhash-dev pkg-config autotools-dev autoconf automake libtool systemd-dev
 
 . /ci-scripts/include.sh
